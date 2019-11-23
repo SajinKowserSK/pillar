81437ca23c8a1a3a44a7e541b2b7aa770b8b62f4
shafinsiddique <siddiqueshafin20@gmail.com>
2019-11-23 08:56:24 -0500

    mongodb working completely now.

--- server/dbHelper.py
+++ server/dbHelper.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
-client = MongoClient("mongodb+srv://shafinsiddique:<Ishafin98>@cluster0-zv1ff.mongodb.net/test?retryWrites=true&w=majority")
+import summarizer
+client = MongoClient("mongodb+srv://shafinsiddique:Ishafin98@cluster0-zv1ff.mongodb.net/test?retryWrites=true&w=majority")
 database = client.get_database("shaman")
 
 class PatientDBHelper:
@@ -10,22 +11,24 @@ class PatientDBHelper:
     def getDataForPatient(self, patientName):
         """Get all the data for patient with 'patientName' """
 
-        return self.collection.findOne({"name":patientName})
+        return self.collection.find_one({"name":patientName})
 
     def addNewPatient(self, name):
         """insert a new patient into the system. This is when the patient
         registers for the first time. Only their name is stored and their medical record
         is empty."""
 
-        self.collection.insertOne({"name":name, "medicalRecord":[]})
+        self.collection.insert_one({"name":name, "medicalRecord":[]})
+        print("{} added to the patient database.".format(name))
 
     def addMedicalDataForPatient(self, patientName, medicalData):
         """append new medical data for the patient with patientName"""
 
         query = {"name":patientName}
-        self.collection.find_one_and_update(query,
-                                                 {"$push": {"date": datetime.now(),
-                                                          "data": medicalData}})
+        self.collection.update_one(query,{"$push": {"medicalRecord": {"date":datetime.now(),"data":medicalData}}})
+        print("Medical Record updated for patient {}".format(patientName))
 
 
-p = PatientDBHelper()
+if __name__ == "__main__":
+    p = PatientDBHelper()
+    print(p.getDataForPatient("Rafit Jamil"))
