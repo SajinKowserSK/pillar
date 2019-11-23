d2635f1d58f29e01070432976e7dc0db79f0258a
rafitj <rafitj@gmail.com>
2019-11-23 04:08:33 -0500

    init flask server and restructure backend

---
+++ server/face_rekog.py
@@ -0,0 +1,67 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import json
+
+def detect_faces(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
+
+    print('Detected faces for ' + photo)    
+    for faceDetail in response['FaceDetails']:
+        print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) 
+              + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
+        print('Here are the other attributes:')
+        print(json.dumps(faceDetail, indent=4, sort_keys=True))
+    return len(response['FaceDetails'])
+
+
+def compare_faces(sourceFile, targetFile):
+
+    client=boto3.client('rekognition')
+   
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+
+    response=client.compare_faces(SimilarityThreshold=80,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+    
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        similarity = str(faceMatch['Similarity'])
+        print('The face at ' +
+               str(position['Left']) + ' ' +
+               str(position['Top']) +
+               ' matches with ' + similarity + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()     
+    return len(response['FaceMatches'])         
+
+def main():
+    # photo='photo'
+    # bucket='bucket'
+    # face_count=detect_faces(photo, bucket)
+    # print("Faces detected: " + str(face_count))
+    data = {}
+    data[]
+    "SourceImage": {
+        "Bytes": "/9j/4AAQSk2Q==..."
+        },
+    "TargetImage": {
+        "Bytes": "/9j/4O1Q==..."
+    },
+    "SimilarityThreshold": 70
+    }
+    source_file='source'
+    target_file='target'
+    face_matches=compare_faces(source_file, target_file)
+    print("Face matches: " + str(face_matches))
+
+
+if __name__ == "__main__":
+    main()
--- server/summarizer.py
+++ server/summarizer.py
@@ -2,10 +2,9 @@ import requests
 API_KEY = "810cac55-0ba9-4ecc-bf32-fb25d6f342dc"
 
 def summarize(text):
-    r = requests.post("https://api.deepai.org/api/summarization",data={'text': text},
-        headers={'api-key': API_KEY}
-
-    ).json()
+    r = requests.post("https://api.deepai.org/api/summarization",
+        data={'text': text},
+        headers={'api-key': API_KEY}).json()
 
     return r['output']
 
