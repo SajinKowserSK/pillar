76b74aaea954af0b63079e7098e8e67af95e5740
RafitJ <rafit.jamil@gmail.com>
2019-11-23 12:43:22 -0500

    fix summarize server

--- backend/app.py
+++ backend/app.py
@@ -4,15 +4,18 @@ from summarizer import summarize
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Hello, World!"
-    
-@app.route('/summarize/', methods = ['GET', 'POST'])
+
+
+@app.route('/summarize/', methods=['GET', 'POST'])
 def summarizer():
     if request.method == 'POST':
         data = request.form
         return (summarize(data["text"]))
+
 
 if __name__ == "__main__":
     app.run(debug=False)
--- backend/summarizer.py
+++ backend/summarizer.py
@@ -1,27 +1,37 @@
-import requests
-from aylienapiclient import textapi
 import boto3
+import requests
+from flask import jsonify
 
-aws_access_key_id = 'AKIAJAPO52PFNOEXBA7Q'
-aws_secret_access_key ='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'
+url = "https://meaningcloud-summarization-v1.p.rapidapi.com/summarization-1.0"
+headers = {
+    'x-rapidapi-host': "meaningcloud-summarization-v1.p.rapidapi.com",
+    'x-rapidapi-key': "f00443165emsh153c5de737def4fp1944f8jsnd6f1569b6c4b",
+    'accept': "application/json"
+}
+
+
+aws_access_key_id = 'AKIAJ5DU42PGNQXHXJGA'
+aws_secret_access_key = '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'
 region = 'us-east-1'
 
-summaryclient = textapi.Client("92fce724", "1ec12ed404cd44234d65fcc666c63f1e")
+aws_medclient = boto3.client(service_name='comprehendmedical',
+                             region_name=region,
+                             aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key)
 
-aws_medclient = boto3.client(service_name='comprehendmedical', 
-                        region_name=region,
-                        aws_access_key_id =  aws_access_key_id,
-                        aws_secret_access_key = aws_secret_access_key )
+aws_compclient = boto3.client(service_name='comprehend',
+                              region_name=region,
+                              aws_access_key_id=aws_access_key_id,
+                              aws_secret_access_key=aws_secret_access_key)
 
-aws_compclient = boto3.client(service_name='comprehend', 
-                        region_name= region,
-                        aws_access_key_id = aws_access_key_id,
-                        aws_secret_access_key = aws_secret_access_key)
+
 def summarize(text):
-    summary = summaryclient.Summarize({'text': text.encode('utf-8'), 'title': "Medical, Disease, Illness", 'sentences_percentage': 50})
-    medical_info = aws_medclient.detect_entities(Text= text)['Entities']
-    sentiment = aws_compclient.detect_sentiment(Text= text, LanguageCode='en')
-    entities = aws_compclient.detect_entities(Text= text, LanguageCode='en')
-    key_pharses = aws_compclient.detect_key_phrases(Text= text, LanguageCode='en')
-    res = {'medical_info': medical_info, 'summary': summary, 'sentiment': sentiment, 'entities': entities}
-    return res
+    querystring = {"txt": text, "sentences": "5"}
+    summary = requests.request(
+        "GET", url, headers=headers, params=querystring).json()['summary']
+    medical_info = aws_medclient.detect_entities(Text=text)['Entities']
+    sentiment = aws_compclient.detect_sentiment(Text=text, LanguageCode='en')
+    entities = aws_compclient.detect_entities(Text=text, LanguageCode='en')
+    res = {'medical_info': medical_info, 'summary': summary,
+           'sentiment': sentiment, 'entities': entities}
+    return jsonify(res)
