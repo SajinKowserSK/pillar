17aec5f7c833f5cf7e79320395417d7b3a5f4539
RafitJ <rafit.jamil@gmail.com>
2019-11-23 18:16:42 -0500

    add patient endpoints and heroku

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-
+from flask import Flask, request, jsonify
+from dbHelper import PatientDBHelper
 from summarizer import summarize
 from face_rekog import face_eval
 app = Flask(__name__)
@@ -23,6 +23,16 @@ def face_match():
         data = request.form
         source, target = data['source'], data['target']
         return face_eval(source, target)
+
+
+@app.route('/patient/', methods=['GET', 'POST'])
+def patient():
+    if request.method == 'GET':
+        name = request.args.get('name')
+        pdb = PatientDBHelper()
+        data = pdb.getDataForPatient(name)
+        res = {'summary': data['medicalRecord'][0]['data']['summary']}
+        return jsonify(res)
 
 
 if __name__ == "__main__":
