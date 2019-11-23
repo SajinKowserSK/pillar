8490789fd8b159203b4caf7ee86327f50b0b7b69
RafitJ <rafit.jamil@gmail.com>
2019-11-23 13:41:52 -0500

    face match finished

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 
 from summarizer import summarize
-
+from face_rekog import compare_faces
 app = Flask(__name__)
 
 
@@ -14,7 +14,15 @@ def home():
 def summarizer():
     if request.method == 'POST':
         data = request.form
-        return (summarize(data["text"]))
+        return summarize(data["text"])
+
+
+@app.route('/face/', methods=['GET', 'POST'])
+def face_match():
+    if request.method == 'POST':
+        data = request.form
+        source, target = data['source'], data['target']
+        return compare_faces(source, target)
 
 
 if __name__ == "__main__":
--- backend/face_rekog.py
+++ backend/face_rekog.py
@@ -1,18 +1,27 @@
-#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
 
 import boto3
 import json
+from flask import jsonify
+
+aws_access_key_id = 'AKIAJ5DU42PGNQXHXJGA'
+aws_secret_access_key = 'PZldHluM3ZPBKSoltMMiLsqN/C2XxZ/HisCPyJY6'
+region = 'us-east-1'
+
+rekog_client = boto3.client(service_name='rekognition',
+                            region_name=region,
+                            aws_access_key_id=aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key)
 
 def detect_faces(photo, bucket):
 
-    client=boto3.client('rekognition')
+    response = rekog_client.detect_faces(
+        Image={'S3Object': {'Bucket': 'shaman-faces', 'Name': photo}}, Attributes=['ALL'])
 
-    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
-
-    print('Detected faces for ' + photo)    
+    print('Detected faces for ' + photo)
     for faceDetail in response['FaceDetails']:
-        print('The detected face is between ' + str(faceDetail['AgeRange']['Low']) 
+        print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
               + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
         print('Here are the other attributes:')
         print(json.dumps(faceDetail, indent=4, sort_keys=True))
@@ -20,47 +29,24 @@ def detect_faces(photo, bucket):
 
 
 def compare_faces(sourceFile, targetFile):
-    client=boto3.client('rekognition')
-   
-    imageSource=open(sourceFile,'rb')
-    imageTarget=open(targetFile,'rb')
 
-    response=client.compare_faces(SimilarityThreshold=80,
-                                  SourceImage={'Bytes': imageSource.read()},
-                                  TargetImage={'Bytes': imageTarget.read()})
-    
+    imageSource = open(sourceFile, 'rb')
+    imageTarget = open(targetFile, 'rb')
+
+    response = rekog_client.compare_faces(SimilarityThreshold=80,
+                                          SourceImage={
+                                              'Bytes': imageSource.read()},
+                                          TargetImage={'Bytes': imageTarget.read()})
+
     for faceMatch in response['FaceMatches']:
         position = faceMatch['Face']['BoundingBox']
         similarity = str(faceMatch['Similarity'])
         print('The face at ' +
-               str(position['Left']) + ' ' +
-               str(position['Top']) +
-               ' matches with ' + similarity + '% confidence')
+              str(position['Left']) + ' ' +
+              str(position['Top']) +
+              ' matches with ' + similarity + '% confidence')
 
     imageSource.close()
-    imageTarget.close()     
-    return len(response['FaceMatches'])         
-
-def main():
-    # photo='photo'
-    # bucket='bucket'
-    # face_count=detect_faces(photo, bucket)
-    # print("Faces detected: " + str(face_count))
-    data = {}
-    data[]
-    "SourceImage": {
-        "Bytes": "/9j/4AAQSk2Q==..."
-        },
-    "TargetImage": {
-        "Bytes": "/9j/4O1Q==..."
-    },
-    "SimilarityThreshold": 70
-    }
-    source_file='source'
-    target_file='target'
-    face_matches=compare_faces(source_file, target_file)
-    print("Face matches: " + str(face_matches))
-
-
-if __name__ == "__main__":
-    main()
+    imageTarget.close()
+    res = {'match': len(response['FaceMatches'])}
+    return jsonify(res)
