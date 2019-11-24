0c77f6f0068231a92b1587e12369f07ba7505534
shafinsiddique <siddiqueshafin20@gmail.com>
2019-11-23 21:55:46 -0500

    updated database structure.

--- backend/dbHelper.py
+++ backend/dbHelper.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from datetime import datetime
 import summarizer
+import csv
 client = MongoClient("mongodb+srv://shafinsiddique:Ishafin98@cluster0-zv1ff.mongodb.net/test?retryWrites=true&w=majority")
 database = client.get_database("shaman")
 
@@ -13,22 +14,34 @@ class PatientDBHelper:
 
         return self.collection.find_one({"name":patientName})
 
-    def addNewPatient(self, name):
+    def addNewPatient(self, name, age, sex, height, weight, bloodtype,  symptoms,
+                      curr_sentiment, numberOfVisits):
+
         """insert a new patient into the system. This is when the patient
         registers for the first time. Only their name is stored and their medical record
         is empty."""
 
-        self.collection.insert_one({"name":name, "medicalRecord":[]})
+        self.collection.insert_one({"name":name, "medicalRecord":[],
+                                    "age":age, "sex":sex,"height":height,
+                                    "weight":weight,
+                                    "blood_type":bloodtype,"prescription":[],
+                                    "number_of_visits":numberOfVisits,
+                                    "curr_symptoms":symptoms,
+                                    "curr_sentiment":curr_sentiment})
+
+
         print("{} added to the patient database.".format(name))
 
     def addMedicalDataForPatient(self, patientName, medicalData):
         """append new medical data for the patient with patientName"""
 
         query = {"name":patientName}
-        self.collection.update_one(query,{"$push": {"medicalRecord": {"date":datetime.now(),"data":summarizer.summarize(medicalData, True)}}})
+        summary = summarizer.summarize(medicalData, True)
+        self.collection.update_one(query,{"$push": {"medicalRecord": {"date":datetime.now(),"data":summary}}})
+        self.collection.update_one(query,{"$inc":{"number_of_visits":1}})
+        self.collection.update_one(query, {"$set":{"curr_sentiment":summary['sentiment']}})
         print("Medical Record updated for patient {}".format(patientName))
 
 
 if __name__ == "__main__":
     p = PatientDBHelper()
-    p.addMedicalDataForPatient("MIKE", "Mrs. Smith also notes that for the past two to three weeks, she has been having intermittent pounding bifrontal headaches that worsen with straining, such as when coughing or having a bowel movement. The headaches are not positional and are not worse at any particular time of day. She rates the pain as 7 or 8 on a scale of 1 to 10, with 10 being the worst possible headache. The pain lessened somewhat when she took Vicodin that she had lying around. ")
